94689fc7c139d729f5ba323d22e2ebcf840a79a8
Harry Garrood <harry@garrood.me>
2020-05-02 18:43:50 +0100

    Allow choosing config at build time (#156)

---
+++ client/config/prod/Try.Config.purs
@@ -0,0 +1,10 @@
+module Try.Config where
+
+loaderUrl :: String
+loaderUrl = "https://compile.purescript.org/output"
+
+compileUrl :: String
+compileUrl = "https://compile.purescript.org"
+
+mainGist :: String
+mainGist = "7ad2b2eef11ac7dcfd14aa1585dd8f69"
