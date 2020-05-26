bf525742c97281cbfdd281d95230bd824e0c0d2d
Thomas R. Honeyman <thoneyman@awakesecurity.com>
2020-05-26 09:47:20 -0700

    Bump React version and add uuid and decimal

--- client/src/Try/Shim.purs
+++ client/src/Try/Shim.purs
@@ -12,19 +12,27 @@ type Shim =
 shims :: Object Shim
 shims = Object.fromFoldable
   [ Tuple "react"
-      { url: "https://unpkg.com/react@16.13.0/umd/react.development.js"
+      { url: "https://unpkg.com/react@16.13.1/umd/react.development.js"
       , deps: []
       }
   , Tuple "react-dom"
-      { url: "https://unpkg.com/react-dom@16.13.0/umd/react-dom.development.js"
+      { url: "https://unpkg.com/react-dom@16.13.1/umd/react-dom.development.js"
       , deps: [ "react" ]
       }
   , Tuple "react-dom/server"
-      { url: "https://unpkg.com/react-dom@16.13.0/umd/react-dom-server.browser.development.js"
+      { url: "https://unpkg.com/react-dom@16.13.1/umd/react-dom-server.browser.development.js"
       , deps: [ "react" ]
       }
   , Tuple "big-integer"
       { url: "https://unpkg.com/big-integer@1.6.48/BigInteger.min.js"
+      , deps: []
+      }
+  , Tuple "decimal.js"
+      { url: "https://unpkg.com/decimal.js@10.2.0/decimal.min.js"
+      , deps: []
+      }
+  , Tuple "uuid"
+      { url: "https://cdnjs.cloudflare.com/ajax/libs/uuid/8.1.0/uuid.min.js"
       , deps: []
       }
   ]
