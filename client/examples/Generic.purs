4b446f24053145c862be1b89ea7e6f726077ea81
Thomas Honeyman <admin@thomashoneyman.com>
2021-05-25 13:12:44 -0700

    Fix generics example

--- client/examples/Generic.purs
+++ client/examples/Generic.purs
@@ -3,9 +3,9 @@ module Main where
 import Prelude
 import Effect.Console (logShow)
 import Data.Generic.Rep (class Generic)
-import Data.Generic.Rep.Eq (genericEq)
-import Data.Generic.Rep.Ord (genericCompare)
-import Data.Generic.Rep.Show (genericShow)
+import Data.Eq.Generic (genericEq)
+import Data.Ord.Generic (genericCompare)
+import Data.Show.Generic (genericShow)
 import TryPureScript (render, withConsole)
 
 data Address = Address
