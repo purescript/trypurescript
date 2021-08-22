857e981db7332934a23948106d82d850f891ed3a
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-22 20:48:52 +0800

    Delete client directory

--- client/src/Try/Container.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-exports.setupIFrame = setupIFrame;
-exports.teardownIFrame = teardownIFrame;
--- client/src/Try/Gist.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-exports.rawUrl_ = function (gistInfo, filename) {
-  if (gistInfo.files && gistInfo.files.hasOwnProperty(filename)) {
-    return gistInfo.files[filename].raw_url;
-  } else {
-    return null;
-  }
-};
