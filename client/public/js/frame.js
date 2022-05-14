683e65835264dd6fcd81675333ae87c5ff920bc6
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-05-14 17:24:03 -0500

    Support shims via es-module-shims

--- client/public/js/frame.js
+++ client/public/js/frame.js
@@ -1,41 +1,20 @@
 (function() {
-  function evalSources(sources) {
-    var modules = {};
-    function dirname(str) {
-      var ix = str.lastIndexOf("/");
-      return ix < 0 ? "" : str.slice(0, ix);
-    }
-    function resolvePath(a, b) {
-      if (b[0] === "." && b[1] === "/") {
-        return dirname(a) + b.slice(1);
-      }
-      if (b[0] === "." && b[1] === "." && b[2] === "/") {
-        return dirname(dirname(a)) + b.slice(2);
-      }
-      return b;
-    }
-    return function load(name) {
-      if (modules[name]) {
-        return modules[name].exports;
-      }
-      function require(path) {
-        return load(resolvePath(name, path));
-      }
-      var module = modules[name] = { exports: {} };
-      new Function("module", "exports", "require", sources[name])(module, module.exports, require);
-      return module.exports;
-    };
-  }
-
   var parent;
 
   document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("message", function(event) {
       parent = event.source;
       parent.postMessage("trypurescript", "*");
-      var file = evalSources(event.data)("<file>");
-      if (file.main && typeof file.main === "function") {
-        file.main();
+      const scriptEl = document.createElement("script");
+      scriptEl.type = "module";
+      const code = event.data;
+      // See https://stackoverflow.com/a/6433770
+      try {
+        scriptEl.appendChild(document.createTextNode(code));
+      } catch (e) {
+        scriptEl.text = code;
+      } finally {
+        document.body.appendChild(scriptEl);
       }
     }, { once: true });
   }, { once: true });
