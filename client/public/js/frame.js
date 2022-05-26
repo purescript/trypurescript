a993bc6b5e9763742acb1733d306dcf779f1d943
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-05-25 20:14:19 -0500

    Stop infinite `main` invocation when DOM modified

--- client/public/js/frame.js
+++ client/public/js/frame.js
@@ -1,22 +1,42 @@
 (function() {
   var parent;
 
-  // Ensures `main` is only called once.
-  this.window.runCount = 0;
-
   document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("message", function(event) {
       parent = event.source;
       parent.postMessage("trypurescript", "*");
+      // `es-module-shims` gets notified every time the DOM
+      // is modified, and it will attempt to run all script
+      // elements with `type="module"`.
+      //
+      // If the code below modifies the DOM in any way (e.g. using
+      // `TryPureScript.render`), `es-module-shims` will reactivate,
+      // which causes it to rerun the script below again.
+      //
+      // To prevent that from happening, we remove the `type="module"`
+      // part from the script element right before the `main` function
+      // is called. The below edits to the compiled JS code
+      // works as follows:
+      // 1. Script element gets added to DOM, activating `es-module-shims`
+      // 2. `es-module-shims` resolves the NPM dependencies and runs the script.
+      // 3. Running the script only schedules the below action.
+      // 4. With the script fully processed and NPM dependencies resolved,
+      //    we remove the `type="module"` part from the script element.
+      // 5. `main` is called. When `es-module-shims` reactivates,
+      //    it sees that the `try-purescript-script-id` element is not
+      //    an ES module and skips it.
+      // 6. Thus, the `main` function is not called an infinite number of times.
+      const scriptId = "try-purescript-script-id";
       const code = `
       ${event.data.code}
-      if (window.runCount === 0) {
+      setTimeout(() => {
+        document.getElementById("${scriptId}").type = "classic";
         main();
-        // bump runCount to ensure we don't re-run this.
-        window.runCount = 1;
-      }`;
+      }, 0);
+      `;
       const scriptEl = document.createElement("script");
       scriptEl.type = "module";
+      scriptEl.id = scriptId;
       scriptEl.appendChild(document.createTextNode(code));
       document.body.appendChild(scriptEl);
     }, { once: true });
