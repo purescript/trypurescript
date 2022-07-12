b5fec9bbb94cfb72e271fbdccbc4fbe4c8e92526
JordanMartinez <jordanalex.martinez@protonmail.com>
2022-07-12 08:59:37 -0500

    Update to PureScript v0.15.4 (#282)
    * Add script to easily update project
    
    * Update to 0.15.4; update package set
    
    * Add changelog
    
    * Update release date

--- client/src/Try/SharedConfig.purs
+++ client/src/Try/SharedConfig.purs
@@ -6,13 +6,13 @@ module Try.SharedConfig where
 import Prelude
 
 pursVersion :: String
-pursVersion = "v0.15.3"
+pursVersion = "v0.15.4"
 
 pursReleaseUrl :: String
 pursReleaseUrl = "https://github.com/purescript/purescript/releases/tag/" <> pursVersion
 
 packageSetVersion :: String
-packageSetVersion = "0.15.2-20220621"
+packageSetVersion = "0.15.2-20220624"
 
 packageSetPackageJsonUrl :: String
 packageSetPackageJsonUrl = "https://github.com/purescript/package-sets/blob/psc-" <> packageSetVersion <> "/packages.json"
--- staging/packages.dhall
+++ staging/packages.dhall
@@ -1,5 +1,5 @@
 let upstream =
-      https://github.com/purescript/package-sets/releases/download/psc-0.15.2-20220621/packages.dhall
-        sha256:78caab14e4d8ff3886a057f0380c2d4a2500e2ee7ab5c1d32a0f9ce5c71eedd8
+      https://github.com/purescript/package-sets/releases/download/psc-0.15.2-20220624/packages.dhall
+        sha256:08989ed9f53e381f879f1b7012ad7684b1ed64d7164c4ad75e306d3210a46c92
 
 in  upstream
