7f5fd390ee083a3d03edb35412f1f32284b6dfe9
Saravanan M <saravanan.m@juspay.in>
2023-03-13 22:37:03 +0530

    fix: Fixed fallback github content url

--- client/config/dev/Try.Config.purs
+++ client/config/dev/Try.Config.purs
@@ -12,4 +12,4 @@ tag :: String
 tag = "master"
 
 mainGitHubExample :: String
-mainGitHubExample = "/purescript/trypurescript/" <> tag <> "/client/examples/Main.purs"
+mainGitHubExample = "purescript/trypurescript/" <> tag <> "/client/examples/Main.purs"
