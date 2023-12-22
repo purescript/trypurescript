7e126854f92f99d6e410bbf0363cc57d886f6638
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-12-21 19:52:27 -0800

    Update to 0.15.13 & latest set

--- client/src/Try/SharedConfig.purs
+++ client/src/Try/SharedConfig.purs
@@ -6,13 +6,13 @@ module Try.SharedConfig where
 import Prelude
 
 pursVersion :: String
-pursVersion = "v0.15.10"
+pursVersion = "v0.15.13"
 
 pursReleaseUrl :: String
 pursReleaseUrl = "https://github.com/purescript/purescript/releases/tag/" <> pursVersion
 
 packageSetVersion :: String
-packageSetVersion = "0.15.9-20230718"
+packageSetVersion = "0.15.13-20231219"
 
 packageSetPackageJsonUrl :: String
 packageSetPackageJsonUrl = "https://github.com/purescript/package-sets/blob/psc-" <> packageSetVersion <> "/packages.json"
